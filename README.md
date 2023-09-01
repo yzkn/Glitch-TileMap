892eee64df5b8acafe97ad9f3041bd8f261472a8
YA <9039417+yzkn@users.noreply.github.com>
2023-09-01 19:33:14 +0900

    Updates

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 ---
 
+https://#####.glitch.me/?lat=35.123456&lon=139.123456
+
+---
+
 Copyright (c) 2023 YA-androidapp(https://github.com/yzkn) All rights reserved.
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+// Copyright (c) 2023 YA-androidapp(https://github.com/YA-androidapp) All rights reserved. 
 
 
 const path = require("path");
@@ -9,10 +9,28 @@ const fastify = require("fastify")({
 const fs = require('fs');
 const https = require('https');
 const jimp = require('jimp');
+ 
 
-
-const L = 85.05112878; // 最大緯度
+const colors = [
+  {mmph: -1, r:  0, g:  0, b:  0, a:  0 },
+  {mmph: -1, r:255, g:255, b:255, a:  0 },
+  {mmph:  0, r:242, g:242, b:255, a:255 },
+  {mmph:  1, r:160, g:210, b:255, a:255 },
+  {mmph:  5, r: 33, g:140, b:255, a:255 },
+  {mmph: 10, r:  0, g: 65, b:255, a:255 },
+  {mmph: 20, r:250, g:245, b:  0, a:255 },
+  {mmph: 30, r:255, g:153, b:  0, a:255 },
+  {mmph: 50, r:255, g: 40, b:  0, a:255 },
+  {mmph: 80, r:180, g:  0, b:104, a:255 }
+];
+const L = ( 180 / Math.PI ) * Math.asin(Math.tanh(Math.PI)); // 85.05112877980663
 const NowcastURL = 'https://www.jma.go.jp/bosai/jmatile/data/nowc/targetTimes_N2.json';
+
+
+const getMmph = (c) => {
+  const color = colors.find(el => el.r == c.r && el.g == c.g && el.b == c.b && el.a == c.a);
+  return color ? color.mmph : -2;
+};
 
 
 const getNowcastJson = () => {
@@ -22,9 +40,9 @@ const getNowcastJson = () => {
       // console.log('statusCode', res.statusCode);
       // console.log('headers', res.headers);
 
-      res.on('data', function (chunk) {
+      res.on('data', function(chunk) {
         result.push(chunk);
-      }).on('end', function () {
+      }).on('end', function() {
         const jsonData = JSON.parse(Buffer.concat(result));
         const data = jsonData
           .map(({ basetime, validtime }) => ({ basetime: basetime, validtime: validtime }))
@@ -43,56 +61,78 @@ const getNowcastJson = () => {
 };
 
 
-const getTileUrl = (lat, lon, zoom) => {
-  lat = parseFloat(lat);
-  lon = parseFloat(lon);
+const getTileUrl = (basetime, validtime, lat, lon, zoom) => {
+  lat  = parseFloat(lat);
+  lon  = parseFloat(lon);
   zoom = parseInt(zoom);
 
   const pixelX = parseInt(Math.pow(2, zoom + 7) * (lon / 180 + 1));
-  const tileX = parseInt(pixelX / 256);
+  const tileX  = parseInt(pixelX / 256);
   const imageX = pixelX % 256;
 
   const pixelY = parseInt((Math.pow(2, zoom + 7) / Math.PI) * ((-1 * Math.atanh(Math.sin((Math.PI / 180) * lat))) + Math.atanh(Math.sin((Math.PI / 180) * L))));
-  const tileY = parseInt(pixelY / 256);
+  const tileY  = parseInt(pixelY / 256);
   const imageY = pixelY % 256;
 
-  const uri = `https://cyberjapandata.gsi.go.jp/xyz/std/${zoom}/${tileX}/${tileY}.png`;
+  const uri = `https://www.jma.go.jp/bosai/jmatile/data/nowc/${basetime}/none/${validtime}/surf/hrpns/${zoom}/${tileX}/${tileY}.png`;
   // console.log('uri', uri)
-  return [uri, imageX, imageY]
+  return [uri, validtime, imageX, imageY]
+};
+
+const getTilePixel = async (uri, validtime, x, y) => {
+  const image = await jimp.read(uri);
+  // console.log(image.bitmap.width, image.bitmap.height);
+    
+  // const buffer = await image.getBufferAsync(jimp.MIME_PNG);
+  // const datauri = 'data:image/png;base64,' + buffer.toString('base64');
+  
+  await image.crop( x, y, 1, 1 );
+  // console.log(image.bitmap.width, image.bitmap.height);
+  
+  const color = await jimp.intToRGBA(image.getPixelColor(0,0));
+  
+  // const buffer = await image.getBufferAsync(jimp.MIME_PNG);
+  // const datauri = 'data:image/png;base64,' + buffer.toString('base64');
+  
+  return {
+    validtime: validtime,
+    mmph: getMmph(color),
+    color: color // ,
+    // image: datauri
+  }
 };
 
 
 fastify.get("/", async (request, reply) => {
-  const lat = request.query.lat ? request.query.lat : 35.681236;
-  const long = request.query.long ? request.query.long : 139.767125;
-  const zoom = request.query.zoom ? request.query.zoom : 10;
-
+  const lat = ( request.query.lat && request.query.lon ) ? request.query.lat : 35.681236;
+  const long = ( request.query.lat && request.query.lon ) ? request.query.lon : 139.767125;
+  const zoom = 10;
+  
   getNowcastJson()
-    .then(json => {
-      console.log('getNowcastJson()', json);
+    .then(async json => {
+    
+        const items = await Promise.all(json.map(async item => {
+          const [uri, vtime, imageX, imageY] = await getTileUrl(item.basetime, item.validtime, lat, long, zoom);
+          return getTilePixel(uri, vtime, imageX, imageY);
+        }))
+
+        // console.log('items', items);
+        reply.send({
+          latitude: lat,
+          longitude: long,
+          // zoom: zoom,
+          items: items
+        })
+    
     })
     .catch(e => {
       console.error(e);
     });
-
-
-  const [uri, imageX, imageY] = getTileUrl(lat, long, zoom)
-  const image = await jimp.read(uri);
-  console.log(image.bitmap.width, image.bitmap.height);
-
-  await image.crop(imageX, imageY, 1, 1);
-  console.log(image.bitmap.width, image.bitmap.height);
-
-  const color = await jimp.intToRGBA(image.getPixelColor(0, 0));
-
-  const buffer = await image.getBufferAsync(jimp.MIME_PNG);
-  console.log('buffer', buffer);
-  const datauri = 'data:image/png;base64,' + buffer.toString('base64');
-
-  reply.code(200)
-    .header('Content-Type', 'application/json')
-    .type('application/json')
-  return { color: color, image: datauri }
+  
+    reply.code(200)
+      .header('Content-Type', 'application/json')
+      .type('application/json')
+    await reply 
 });
 
 
